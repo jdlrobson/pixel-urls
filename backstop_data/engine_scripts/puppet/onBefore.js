e70c8729789af84a3d957f865e3269c4d345ab86
Jon Robson <jdlrobson@gmail.com>
2025-12-09 16:13:40 -0800

    Add important backstop data

---
+++ backstop_data/engine_scripts/puppet/onBefore.js
@@ -0,0 +1,4 @@
+module.exports = async (page, scenario, vp) => {
+  await require('./loadCookies')(page, scenario);
+  await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 18_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Mobile/15E148 Safari/604.1');
+};
